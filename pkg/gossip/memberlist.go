e4166f63ed726b02aab27061861bbdeaeb2f4b97
Ryan Dielhenn <dielhennr@gmail.com>
2025-09-14 09:22:28 -0700

    message-memberlist-sketch

--- pkg/gossip/memberlist.go
+++ pkg/gossip/memberlist.go
@@ -1,6 +1,24 @@
 package gossip
 
+import "time"
+
 // Tracks the cluster's view of active members
 // Data structures for Member (id, address, state, incarnation number)
 // Add/remove/update logic
 // Maybe a background routine that spreads membership rumors
+
+type Member struct {
+    ID          NodeID    // unique, usually UUID or host:port string
+    Addr        string    // current reachable address
+    Incarnation uint64    // version number for this member’s state
+    State       State     // Alive, Suspect, Dead
+    LastUpdate  time.Time // (optional) for metrics/GC
+}
+
+type MemberList interface {
+    Self() Member
+    All() []Member
+    Get(id NodeID) (Member, bool)
+    ApplyDelta(d Delta) bool
+    BumpIncarnation() uint64
+}
--- pkg/gossip/message.go
+++ pkg/gossip/message.go
@@ -3,3 +3,35 @@ package gossip
 // Defenitions of the wire protocol: heartbeat messages, membership updates, suspicion signals etc.
 // Likely small structs with Matshal/Unmarhsal helpers
 // Keeps network encoding/decoding logic isolated
+type NodeID string
+
+type State uint8
+const (
+    StateAlive State = iota
+    StateSuspect
+    StateDead
+)
+
+type MsgType uint8
+const (
+    MsgPing MsgType = iota
+    MsgAck
+    MsgIndirectPing
+    MsgPushPullReq
+    MsgPushPullResp
+)
+
+type Delta struct {
+    Member Member
+    // Optional: tombstone deadline for StateDead to garbage-collect
+}
+
+type GossipMsg struct {
+    Type       MsgType
+    From       NodeID
+    Target     *NodeID          // for indirect pings
+    Deltas     []Delta          // piggyback
+    Nonce      uint64           // match ping/ack
+    SchemaV    uint16           // forward-compat
+    // Optional: HMAC/nonce for auth later
+}
